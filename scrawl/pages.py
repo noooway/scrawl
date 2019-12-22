9534071cb80e22b89102dff7e95f517a8f7a4594
andrew <andrew@dell.domainname>
2019-12-22 17:20:07 +0300

    Basic 'Create Page' functionality

--- scrawl/pages.py
+++ scrawl/pages.py
@@ -1,7 +1,8 @@
 import os
 from flask import (
+    current_app,
     Blueprint, render_template, request,
-    current_app
+    redirect, url_for, safe_join, flash
 )
 
 
@@ -19,17 +20,31 @@ def index():
 def render_page(filename):
     path = current_app.config['PAGESPATH']
     if request.method == 'POST':
+        # todo: security
         content = request.form['content']
         print(content)
-        fullpath = os.path.join(path, filename)
+        fullpath = safe_join(path, filename)
         with open(fullpath, 'w') as f:
             f.write(content)
     pages = get_pages(path)
     current_page = filename
-    fullpath = os.path.join(path, filename)
+    fullpath = safe_join(path, filename)
     with open(fullpath, 'r') as f:
         content = f.read()
     return render_template('base.html', pages=pages, content=content)
+
+
+@bp.route('/create_page', methods=['POST'])
+def create_page():
+    # todo: security;
+    # http://lucumr.pocoo.org/2010/12/24/common-mistakes-as-web-developer/
+    pagename = request.form['page_name']
+    path = current_app.config['PAGESPATH']
+    fullpath = safe_join(path, pagename)
+    with open(fullpath, "a+") as f:
+        create_text = '<div id="content">\n Scrawl! \n</div>'
+        f.write(create_text)
+    return redirect(url_for('pages.render_page', filename=pagename))
 
 
 def get_pages(path):
